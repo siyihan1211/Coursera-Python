3f260808b91aeb613b75f8f383ef633e27da3672
siyihan1211 <siyihan1211@gmail.com>
2025-09-01 20:04:28 -0500

    exercise 3.3

--- Codes/Py4e_hw3.1.py
+++ Codes/Py4e_hw3.1.py
@@ -3,12 +3,14 @@
 #Use 45 hours and a rate of 10.50 per hour to test the program (the pay should be 498.75). 
 #You should use input to read a string and float() to convert the string to a number. 
 #Do not worry about error checking the user input - assume the user types numbers properly.
+
 hrs = input("Enter Hours:")
 h = float(hrs)
 rate = input ("Enter Rate")
 r = float(rate)
-pay = h * r
+
 if h <= 40:
+    pay = h * r
     print(pay)
 else:
     pay = 1.5 * r * (h - 40) + (40 * r)
